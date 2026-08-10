c9409f67ada024c2324fac52641c78275716166d
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-10 15:39:42 -0400

    Output meal images with unknown dimensions, using the fill prop.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
@@ -10,13 +11,15 @@ export default function MealsPage() {
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>
-          Choose your favorite recipe and cook it yourself. It is easy and fin!
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
-          <Link href="/meals/share">Shhare your favorite recipe</Link>
+          <Link href="/meals/share">Share your favorite recipe</Link>
         </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
