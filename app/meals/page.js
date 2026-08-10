3c96f3402171fce70a65563aa24eda1b64b129c2
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-10 15:29:02 -0400

    Output meals data and add meals page content.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fin!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Shhare your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }
