94a2b399498b59517b5a8a5ab09f9e173d02d493
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-11 17:44:31 -0400

    Improving the picker.

--- app/meals/share/image-picker.js
+++ app/meals/share/image-picker.js
@@ -16,6 +16,7 @@ export default function ImagePicker({ label, name }) {
     const file = event.target.files[0];
 
     if (!file) {
+      setPickedImage(null);
       return;
     }
 
@@ -50,6 +51,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
