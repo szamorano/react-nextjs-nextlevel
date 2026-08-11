cff8d22404fc30e5763f60b67d456c56a1f73cfd
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-11 11:49:40 -0400

    Prepared the MealDetailsPage for data.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,27 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
 export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: "..." }}
+        ></p>
+      </main>
+    </>
+  );
 }
