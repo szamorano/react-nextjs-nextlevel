c2e2bd2ba6cd69f6c32906f60543d227b06681f4
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-11 13:30:40 -0400

    Output all the other data.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -5,26 +5,27 @@ import { getMeal } from "@/lib/meals";
 export default async function MealDetailsPage({ params }) {
   const { mealSlug } = await params;
   const meal = getMeal(mealSlug);
-  console.log(meal);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill />
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}>SUMMARY</p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
         <p
           className={classes.instructions}
-          dangerouslySetInnerHTML={{ __html: "..." }}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
         ></p>
       </main>
     </>
